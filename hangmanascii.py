f882fdc01bee49a325f439834f1343066282276b
thewachdog <75667703+thewachdog@users.noreply.github.com>
2021-09-12 18:09:55 +0530

    Create hangmanascii.py

---
+++ hangmanascii.py
@@ -0,0 +1,62 @@
+def title():
+        print('''
+         _   _
+        | | | | __ _ _ __   __ _ _ __ ___   __ _ _ __
+        | |_| |/ _` | '_ \\ / _` | '_ ` _ \\ / _` | '_ \\
+        |  _  | (_| | | | | (_| | | | | | | (_| | | | |
+        |_| |_|\\__,_|_| |_|\\__, |_| |_| |_|\\__,_|_| |_|
+                           |___/
+        ''')
+
+
+hangman = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''','''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''','''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''']
+wordlist = ['provoke', 'tiptoe', 'button', 'sword', 'continuous', 'social', 'coverage', 'expenditure', 'cut', 'magnetic', 'queue', 'representative', 'infinite', 'adopt', 'smell', 'gear', 'inflation', 'need', 'bring', 'negative', 'variation', 'hotdog', 'forum', 'feminine', 'god', 'cabin', 'coincide', 'chocolate', 'prove', 'quote', 'voice', 'witness', 'course', 'kit', 'path', 'jealous', 'systematic', 'question', 'fantasy', 'benefit', 'allow', 'depend', 'marriage', 'intelligence', 'red', 'polite', 'pig', 'oil', 'concern', 'scale']
